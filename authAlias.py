4a17dec132955f376fdbc069696225877275a54c
root <root@ibm-im-was-dmgr01.(none)>
2016-12-02 16:17:19 +0000

    auth Alias Connection Factories veryfied ..

--- authAlias.py
+++ authAlias.py
@@ -1,4 +1,10 @@
 import sys
+def exist():
+	listAuth = AdminTask.listAuthDataEntries().split('\n')
+	for list in listAuth:
+	   if aliasName == list.split('alias')[1].split(']')[0].strip() :
+		return list.split('alias')[1].split(']')[0].strip()
+#Exist Method
 def createAuthAlias(aliasName,user,password): 
        print "Creating Auth Alias" 
        security = AdminConfig.getid('/Security:/') 
@@ -7,11 +13,14 @@ def createAuthAlias(aliasName,user,password):
        pw = ['password', password] 
        jaasAttrs = [alias, userid, pw] 
        aliasId = AdminConfig.create('JAASAuthData', security, jaasAttrs) 
-       return(aliasId) 
+       print aliasId 
 
 aliasName = sys.argv[0]
 user = sys.argv[1]
 password = sys.argv[2]
 if len(sys.argv[0:]) == 3:
+    if aliasName == exist():	
+	print 'Autheticaltion Alias with the name'+aliasName+' specified already exist'
+    else:
 	createAuthAlias(aliasName,user,password)
 	AdminConfig.save()
