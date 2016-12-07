2c836984b3aeda18d142f4e87b928e19b26c78b5
cw22dc3 <cw22dc3@4S110-BK3THM1.fepoc.com>
2016-12-07 14:14:34 -0500

    CFC wORKING cOPY

--- createCFC.py
+++ createCFC.py
@@ -1,8 +1,8 @@
-
 import os
 import sys
-
-def createCF(CFName, QMName, CFJNDI='jndi/'+CFName, qmgrSvrConnChan):    
+def createCF(CFName, QMName, qmgrSvrConnChan='', CFJNDI=''):    
+    if CFJNDI:
+        CFJNDI = 'jndi/'+CFName
     CF_cmd =  '[-type CF -name '+CFName
     CF_cmd+=  ' -jndiName '+CFJNDI
     if qmgrHost != '':
@@ -17,12 +17,18 @@ def createCF(CFName, QMName, CFJNDI='jndi/'+CFName, qmgrSvrConnChan):
         CF_cmd+=   ' -connectionNameList '+ConnNameList+' ]'
     elif ClChanDefTabURL != '':
         CF_cmd+= ' -description -ccdtUrl '+ClChanDefTabURL+' -ccdtQmgrName '+QMName+' ]'
+    print 'Running Admin Command with params'+Provider,CF_cmd
+    CFCMDRUN = AdminTask.createWMQConnectionFactory(Provider, CF_cmd )
+    AdminConfig.save()
+    # createCF Method Completed
+qmgrSvrConnChan = ''
+qmgrHost=''
+ConnNameList=''
+ClChanDefTabURL=''
 
-    AdminTask.createWMQConnectionFactory(Provider, CF_cmd )
-# createCF Method Completed
 
 args = sys.argv[0:]
-if len(args) >= 3 :
+if len(args) >= 4 :
     for arg in args:
         key, value = arg.split('=')
         if  key == 'cell':
@@ -44,15 +50,18 @@ if len(args) >= 3 :
             qmgrSvrConnChan = value
         if  key == 'qmgrHost':
             qmgrHost = value
-        if  key ==  'qmgrPortNumber':
+        if  key ==  'qmgrPort':
             qmgrPortNumber = value
         if  key == 'ConnNameList':
             ConnNameList = value
         if  key == 'ClChanDefTabURL':
             ClChanDefTabURL = value
-    if CFName or QMName or CFJNDI or scope:
+    if CFName or QMName  or CellScope or qmgrHost:
+        print CellScope,ClusScope,CFName,QMName,CFJNDI,qmgrHost
+        createCF(CFName, QMName ,qmgrSvrConnChan , CFJNDI) 
+    else:
         print 'These variables should not be empty script CFName or QMName or CFJNDI or Cell(scope of WebSphere) '
-    createCF(CFName, QMName, CFJNDI, qmgrSvrConnChan)
+        
 else:    
     print 'Usage: scriptName cell=CellName (Node=name or Cluster=clusterName ) CFName=name  QMName=QM CFJNDI=JNDI qmgrSvrConnChan=CHANNEL.OUT (qmgrHost=HOST qmgrPort=1212 or ConnNameList=CHN1,CH2 or ClChanDefTabURL="http://"'   
         
