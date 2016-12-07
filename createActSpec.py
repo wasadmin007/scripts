beb19b8f5ce01aa69d2e2a885b1116a136005156
cw22dc3 <cw22dc3@4S110-BK3THM1.fepoc.com>
2016-12-07 14:15:05 -0500

    Activation Specification

---
+++ createActSpec.py
@@ -0,0 +1,6 @@
+
+
+
+AdminTask.createWMQActivationSpec('"WebSphere MQ JMS Provider(cells/cell/nodes/node01|resources.xml#builtin_mqprovider)"', '[-name sadf -jndiName sdfsa -description -destinationJndiName asdf -destinationType javax.jms.Queue -messageSelector asdf -qmgrName sadf -wmqTransportType BINDINGS_THEN_CLIENT -qmgrSvrconnChannel -qmgrHostname asdf -qmgrPortNumber 1212 ]')
+
+  
